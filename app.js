8962f82cef5fe955ef10c1aa6b4c9a5fd03ff223
Danielo64 <danielpeassuncao@gmail.com>
2024-08-20 17:52:25 -0300

    curso2

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Jogo do Número Secreto";
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela("h1", "Jogo do número secreto");
+exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 
 function verificarChute() {
-    console.log("O botão foi clicado!");
+    let chute = document.querySelector("input").value;
+    console.log(chute == numeroSecreto);
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela("h1", "Acertoooou!");
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}! :)`;
+        exibirTextoNaTela("p", mensagemTentativas);
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela("p", "O número secreto é menor...");
+        } else {
+            exibirTextoNaTela("p", "O número secreto é maior...");
+        }
+        tentativas++;
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
